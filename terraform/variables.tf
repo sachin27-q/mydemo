6cf9ca4f7a1efe6e42db8912b9babcbc267399c3
sachin27-q <office2@mykcpl.com>
2025-09-09 10:02:13 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type    = string
+  default = "demo1-471009"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
+variable "gke_machine_type" {
+  type    = string
+  default = "e2-small"
+}
